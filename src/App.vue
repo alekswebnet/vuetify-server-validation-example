8dc8539b0e5fcefe42555b8a51d2c716c221cff6
Oleksandr Shevchuk <34628096+alekswebnet@users.noreply.github.com>
2023-04-01 05:01:24 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <Form />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import Form from '@/components/Form.vue'
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,73 @@
+<template>
+  <form class="pt-6" @submit.prevent="submit">
+    <legend class="text-h4 mb-6">Login form</legend>
+    <v-text-field
+      v-model="email.value.value"
+      class="me-10"
+      :error-messages="email.errorMessage.value"
+      label="E-mail"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password.value.value"
+      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+      :type="showPassword ? 'text' : 'password'"
+      class="mb-4"
+      label="Password"
+      counter
+      :error-messages="password.errorMessage.value"
+      @click:append="showPassword = !showPassword"
+    ></v-text-field>
+
+    <v-btn
+      class="me-4"
+      type="submit"
+    >
+      submit
+    </v-btn>
+
+    <v-btn @click="handleReset">
+      clear
+    </v-btn>
+  </form>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import { useField, useForm } from 'vee-validate'
+  import { useFetch } from '@vueuse/core'
+
+  export default {
+    setup () {
+      const { handleSubmit, handleReset, setErrors } = useForm()
+      const email = useField('email')
+      const password = useField('password')
+      const showPassword = ref(false)
+
+      const submit = handleSubmit(async values => {
+        const body = JSON.stringify({ 
+          email: values.email, 
+          password: values.password 
+        })
+        const { data } = await useFetch('https://reqres.in/api/register', {
+          method: 'POST',
+          headers: { 
+            'Content-Type': 'application/json' 
+          },
+          body
+        }).json()
+        
+        if (data.value.error) {
+          const isPasswordError = data.value.error.includes('password')
+          return setErrors({
+            email: !isPasswordError && data.value.error,
+            password: isPasswordError && data.value.error
+          })
+        }
+        alert(JSON.stringify(data.value))
+      })
+
+      return { email, password, showPassword, submit, handleReset }
+    },
+  }
+</script>
